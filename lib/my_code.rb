b929aa78199c37f7610ed5b3bdfc42353c834a53
Graham Flaspoehler <grahamflas@gmail.com>
2019-08-25 13:10:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# My Code here....
+def map_to_negativize(array)
+    new_array = Array.new
+
+    (array.length).times do |index|
+        new_array << array[index] * -1
+    end
+
+    return new_array
+end
