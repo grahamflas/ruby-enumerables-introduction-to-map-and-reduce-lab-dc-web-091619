29cb4235c1784de9cb7a9311454a32d162b72a0b
Graham Flaspoehler <grahamflas@gmail.com>
2019-08-25 13:15:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,13 @@ def map_to_no_change(array)
 
     return new_array
 end
+
+def map_to_double(array)
+    new_array = Array.new
+
+    (array.length).times do |index|
+        new_array << array[index] * 2
+    end
+
+    return new_array
+end
